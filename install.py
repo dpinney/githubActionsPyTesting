9f0258adec6fa1a67f074338b1831ae646ee2d3d
David Pinney <david@pinney.org>
2020-11-22 09:12:25 -0500

    Update install.py

--- install.py
+++ install.py
@@ -18,7 +18,7 @@ if platform.system()=="Darwin": # MacOS
 
 if platform.system()=="Windows":
 	os.system("choco install -y wget")
-	# os.system("choco install -y vcredist-all")
+	os.system("choco install -y vcredist-all")
 	os.system("wget --no-check-certificate https://sourceforge.net/projects/gridlab-d/files/gridlab-d/Candidate%20release/gridlabd-4.0_RC1.exe")
 	os.system("gridlabd-4.0_RC1.exe/silent")
 	# time.sleep(10)

d5ba3ca4d7066304f2912d0cb4475e6592fb4af2
David Pinney <david@pinney.org>
2020-11-22 16:30:44 +0000

    removing extraneous libs

--- install.py
+++ install.py
@@ -18,10 +18,10 @@ if platform.system()=="Darwin": # MacOS
 
 if platform.system()=="Windows":
 	os.system("choco install -y wget")
-	os.system("choco install -y vcredist-all")
-	os.system("choco install -y mingw")
-	os.system("choco install -y cygwin")
-	os.system("choco install -y MSYS2")
+	# os.system("choco install -y vcredist-all")
+	# os.system("choco install -y mingw")
+	# os.system("choco install -y cygwin")
+	# os.system("choco install -y MSYS2")
 	os.system("wget -nv --no-check-certificate https://sourceforge.net/projects/gridlab-d/files/gridlab-d/Candidate%20release/gridlabd-4.0_RC1.exe")
 	os.system("gridlabd-4.0_RC1.exe/silent")
 	# time.sleep(10)

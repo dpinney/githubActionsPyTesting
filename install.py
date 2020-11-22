6f9e7d4ca7ef07916a5a50c2710863075de8a5a9
David Pinney <david@pinney.org>
2020-11-22 08:50:09 -0500

    Add files via upload

---
+++ install.py
@@ -0,0 +1,29 @@
+import platform, os, time
+#  install script
+print('INSTALL RUNNING')
+
+# macos brew testing
+if platform.system()=="Darwin": # MacOS
+	# os.system("brew install wget ffmpeg git graphviz octave mdbtools") # fails, kills python
+	# os.system("brew install wget ffmpeg git graphviz octave mdbtools; brew link --overwrite python3") # works, extra 5 minutes
+	os.system("HOMEBREW_NO_AUTO_UPDATE=1 brew install wget ffmpeg git graphviz octave mdbtools") # works, fast
+	# SPLAT TEST
+	os.system("wget https://www.qsl.net/kd2bd/splat-1.4.2-osx.tgz")
+	os.system("sudo tar -xvzf splat-1.4.2-osx.tgz")
+	os.system('''
+		cd splat-1.4.2;
+		sed -i '' 's/ans=""/ans="2"/g' configure;
+		sudo bash configure;
+	''')
+
+if platform.system()=="Windows":
+	os.system("choco install -y wget")
+	# os.system("choco install -y vcredist-all")
+	os.system("wget --no-check-certificate https://sourceforge.net/projects/gridlab-d/files/gridlab-d/Candidate%20release/gridlabd-4.0_RC1.exe")
+	os.system("gridlabd-4.0_RC1.exe/silent")
+	# time.sleep(10)
+	# os.system('powershell "./gridlabd-4.0_RC1.exe /silent"')
+	# os.system("choco install -y mingw")
+	# os.system("choco install -y cygwin")
+	# os.system("choco install -y gnuplot")
+	# os.system("gridlabd-4.0_RC1.exe /silent")
---
+++ script.py
@@ -0,0 +1,39 @@
+# testing script
+import platform, os, subprocess
+print('TEST SCRIPT RUN')
+
+def safe_call(cmdString):
+	try:
+		output = subprocess.check_output(
+			cmdString, stderr=subprocess.STDOUT, shell=True,
+			universal_newlines=True)
+	except subprocess.CalledProcessError as exc:
+		print(str(cmdString) + " Status : FAIL", exc.returncode, exc.output)
+	else:
+		print(str(cmdString) + " Output: \n{}\n".format(output))
+
+'''
+GRIDLABD FAIL
+-------------
+Use command to get required dlls? They're in there.
+Create a windows server test env on azure. Windows Server, version 1809. Tried on 1909... worked fine. ARGH!!!
+PULL BINARIES!?!?!?! Seems like a longshot since everything's "fine"
+Switch to... github actions????
+Turns out, running inside command with refreshenv first works. Now, how to do this more generally... just doing it in travis hangs, might need a batch file.
+'''
+
+if platform.system()=="Windows":
+	print('WINDOWS DETECTED')
+	# safe_call('cmd.exe //c RefreshEnv.cmd') #hangs travis
+	# print('PATH', os.getenv('PATH'))
+	# print('GLPATH', os.getenv('GLPATH'))
+	# print(os.listdir('C:\\Program Files\\GridLAB-D\\bin'))
+	# print(os.listdir('C:\\Program Files\\GridLAB-D\\lib'))
+	# print(os.listdir('C:\\windows\\system32'))
+	# safe_call(['env'])
+	safe_call(['gridlabd', '-h'])
+	# safe_call('cmd /c "refreshenv && gridlabd -h"')
+	# safe_call('cmd /c "gridlabd -h"')
+	safe_call(['gridlabd', 'smsSingle.glm'])
+	# safe_call('cmd /c "refreshenv && gridlabd smsSingle.glm"')
+	# safe_call('cmd /c "refreshenv && SET"')
